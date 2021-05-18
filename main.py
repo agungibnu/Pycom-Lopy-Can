fa6e2a803a8b67b5cc69d3c169a7748f9205067d
agungibnu <agung.ibnughani@gmail.com>
2021-05-18 16:29:23 +0700

    main-program-lopy-logger

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ def can_cb(can_o):
     print('CAN Rx:', can_o.recv())
 
 can.callback(handler=can_cb, trigger=CAN.RX_FRAME)
+
+
+#For example:
+
+#can.soft_filter(CAN.FILTER_LIST, [0x100, 0x200, 0x300, 0x400])  # only accept identifiers from 0x100, 0x200, 0x300 and 0x400
+
+#can.soft_filter(CAN.FILTER_RANGE, [(0x001, 0x010), (0x020, 0x030), (0x040, 0x050)])  # only accept identifiers from 0x001 to 0x010, from 0x020 to 0x030 and from 0x040 to 0x050.
+
+#can.soft_filter(CAN.FILTER_MASK, [(0x100, 0x7FF), (0x200, 0x7FC)]) # more of the classic Filter and Mask method.
+
+#can.soft_filter(None)   # disable soft filters, all messages are accepted
