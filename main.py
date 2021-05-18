e7b4afeedf57ec493d604ff2d965ee3cfe6a68f5
agungibnu <agung.ibnughani@gmail.com>
2021-05-18 16:09:46 +0700

    new

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # Coba LoPy Read Canbus Arduino
 # dokumentasi : https://docs.pycom.io/firmwareapi/pycom/machine/can/#constructors
-
-import machine
-from machine import CAN
-
+import utime
+import pycom
+  pycom.heartbeat(False)
+pycom.rgbled(0xf00)
+# pycom.pybytes_on_boot(False)
 
 # Init CANBUS
 
-canbus = CAN(mode=CAN.NORMAL, baudrate=500000, pins=('P22', 'P23'))
+can = CAN(mode=CAN.NORMAL, baudrate=500000, pins=('P22', 'P23'))
 
 
-def can_callback(can_o):
-    print('Canbus RX : ', can_o.recv())
+def can_cb(can_o):
+    utime.sleep(1)
+    print('CAN Rx:', can_o.recv())
 
-
-canbus.callback(handler=can_callback, trigger=CAN.RX_FRAME)
+can.callback(handler=can_cb, trigger=CAN.RX_FRAME)
